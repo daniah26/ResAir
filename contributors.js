c3c7a481ad2e6e11e66f16778cf29bdabbe09581
lubna28l <lubna418@hotmail.com>
2025-11-10 23:03:44 +0300

    Update contributors.js style and coments

--- contributors.js
+++ contributors.js
@@ -1,6 +1,6 @@
 const repoOwner = "daniah26";
 const repoName = "ResAir";
-
+// updated by lubna 
 async function fetchContributors() {
   const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`);
   const data = await response.json();
@@ -12,14 +12,16 @@ async function fetchContributors() {
     list.innerHTML = "<li>No contributors found.</li>";
     return;
   }
-
+<p id="contributor-count" style="font-weight:bold; margin-bottom:8px;"></p>
+<ul id="contributors"></ul>
   data.forEach(contributor => {
     const li = document.createElement("li");
     li.innerHTML = `
-      <img src="${contributor.avatar_url}" width="30" style="border-radius:50%; vertical-align:middle; margin-right:8px;">
+      <img src="${contributor.avatar_url}" width="40" style="border-radius:55%; vertical-align:middle; margin-right:8px;">
       <a href="${contributor.html_url}" target="_blank">${contributor.login}</a>
     `;
     list.appendChild(li);
+    
   });
 }
 
